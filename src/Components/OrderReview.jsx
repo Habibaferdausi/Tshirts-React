c2a2aaa83dc6d6bdf3eb3d2be5f32568201e8c82
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-08 22:12:57 +0600

    review update

--- src/Components/OrderReview.jsx
+++ src/Components/OrderReview.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const OrderReview = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is review page!!!!!!!!</h1>
+    </div>
+  );
 };
 
 export default OrderReview;
