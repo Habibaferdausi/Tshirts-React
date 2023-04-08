c47b94a554242b7890cb422613f9f018746be890
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-08 22:06:37 +0600

    updateorder

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -7,10 +7,7 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
   } else {
     message = (
       <div>
-        <h3>Boroloxxx</h3>
-        <p>
-          <small>Thanks for giving your money</small>
-        </p>
+        <h3>You have succesfully added this </h3>
       </div>
     );
   }
@@ -19,14 +16,10 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
       <h2 className={cart.length === 1 ? "blue " : " red"}>
         Order Summary: {cart.length}
       </h2>
-      <p className={`bold bordered ${cart.length === 3 ? "yellow" : "purple"}`}>
-        something
-      </p>
-      {cart.length > 2 ? (
-        <span className="purple">Aro kino</span>
-      ) : (
-        <span>fokira</span>
-      )}
+      <p
+        className={`bold bordered ${cart.length === 3 ? "yellow" : "purple"}`}
+      ></p>
+      {cart.length > 2 ? <span className="purple"></span> : <span></span>}
       {message}
       {cart.map((tshirt) => (
         <p key={tshirt._id}>
@@ -34,8 +27,8 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
           <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button>
         </p>
       ))}
-      {cart.length === 2 && <p>Double bonanza!!!</p>}
-      {cart.length === 3 || <h3>Tinta to hoilo na!!</h3>}
+      {cart.length === 2 && <p></p>}
+      {cart.length === 3 || <h3> </h3>}
     </div>
   );
 };
